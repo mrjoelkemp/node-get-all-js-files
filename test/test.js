df0c63ad3e8b9dfd3bd210a60de1e4c6c27cae09
Joel Kemp <jokemp@adobe.com>
2014-10-07 15:30:51 -0400

    Supports exclusions

--- test/test.js
+++ test/test.js
@@ -1,6 +1,7 @@
 var getJSFiles = require('../'),
     assert = require('assert'),
-    path = require('path');
+    path = require('path'),
+    sinon = require('sinon');
 
 var dir = __dirname + '/example';
 
@@ -12,12 +13,36 @@ describe('lib/getJSFiles', function() {
         var withJSExt = files.filter(function(filename) {
           return path.extname(filename) === '.js';
         });
-        assert(files.length === 2);
+        assert(files.length === 3);
         assert(files.length === withJSExt.length);
         done();
       }
     });
   });
 
-  it.skip('calls the contentCb for each file');
+  it('calls the contentCb for each file', function(done) {
+    var spy = sinon.spy();
+
+    getJSFiles({
+      directory: dir,
+      contentCb: spy,
+      filesCb: function(files) {
+        assert(spy.called);
+        done();
+      }
+    });
+  });
+
+  it('supports excluding directories', function(done) {
+    getJSFiles({
+      directory: dir,
+      dirOptions: {
+        excludeDir: /subdir/
+      },
+      filesCb: function(files) {
+        assert(files.length === 2);
+        done();
+      }
+    });
+  });
 });
